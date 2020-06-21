38d5aecb11a7008203ab470fc7b97f3e65a5c879
Abhishek Biswas <64304355+codebyabhishek772@users.noreply.github.com>
2020-06-21 20:24:24 +0530

    Add files via upload

---
+++ Basic Calculator.java
@@ -0,0 +1,23 @@
+package Arithmetic;
+
+public class Maths {
+
+ public static void main(String args[])
+{
+	int num1 = 5, num2 =7, Result1, Result2, Result3, Result4;
+
+	Result1 = num1 +num2;
+	Result2 = num1 * num2;
+	Result3 = num2 / num1;
+	Result4 = num2- num1;
+	float Result5 = num2 % num1;
+	
+	System.out.println("ADDATION RESULT = " + Result1);
+	System.out.println("MULTIPICATION RESULT = " + Result2);
+	System.out.println("DIVISION RESULT = " + Result3);	
+	System.out.println("SUBTRACTION RESULT = " + Result4);
+	System.out.println("MODULO RESULT = " + Result5);
+	
+ }
+
+}
